63e4df83d11cfd851762f8f8234616da9a179a9f
mikaylapradeepan <mikaylapradeepan@gmail.com>
2023-10-23 10:58:04 -0400

    "remove weird header"

--- src/view/in_battle/AttackResultView.java
+++ src/view/in_battle/AttackResultView.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:src/view/AttackResultView.java
-package view;
-=======
 package view.in_battle;
->>>>>>> 8a81a4e8365ad1cf2b2d24a76879b5e57d1aaceb:src/view/in_battle/AttackResultView.java
 
 public class AttackResultView {
 }
--- src/view/in_battle/AttackSelectionView.java
+++ src/view/in_battle/AttackSelectionView.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:src/view/AttackSelectionView.java
-package view;
-=======
 package view.in_battle;
->>>>>>> 8a81a4e8365ad1cf2b2d24a76879b5e57d1aaceb:src/view/in_battle/AttackSelectionView.java
 
 public class AttackSelectionView {
 }
