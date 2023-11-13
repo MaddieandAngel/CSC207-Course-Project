a74575a85bb178da9036a77256f6a46583e2f8f4
MaddieandAngel <madeleine.morais@mail.utoronto.ca>
2023-11-13 11:00:37 -0500

    Overhauled Player and Enemy classes (and the enemy factory), turning them into interfaces and adding ActivePlayer and CurrentEnemy to replace the original classes. Started implementing the InBattleDataAccessObject. The Character interface can now probably be deleted, but it has been left intact for now, just in case.

--- src/entity/Enemy.java
+++ src/entity/Enemy.java
@@ -1,85 +1,11 @@
 package entity;
 
-public class Enemy implements Character{
-
-    private String name;
-    private int currentHealth;
-    private int level;
-    private char suitPreference;
-    private int minCardValue;
-    private int maxCardValue;
-    private String image;
-    //private ArrayList<Card> hand;
-
-    public Enemy(int id, int level){
-        this.level = level;
-        currentHealth = level * 15;
-
-        if (id == 0){ //Clubs mage
-            name = "Clubs Mage";
-            suitPreference = 'C';
-            minCardValue = 0;
-            maxCardValue = 15;
-        }
-        else if (id == 1){ //Spades mage
-            name = "Spades Mage";
-            suitPreference = 'S';
-            minCardValue = 0;
-            maxCardValue = 15;
-        }
-        else if (id == 2){ //Hearts mage
-            name = "Hearts Mage";
-            suitPreference = 'H';
-            minCardValue = 0;
-            maxCardValue = 15;
-        }
-        else if (id == 3){ //Diamonds mage
-            name = "Diamonds Mage";
-            suitPreference = 'D';
-            minCardValue = 0;
-            maxCardValue = 15;
-        }
-        else if (id == 4){ //Lesser omni mage
-            name = "Lesser Omni Mage";
-            suitPreference = '\0';
-            minCardValue = 1;
-            maxCardValue = 7;
-        }
-        else if (id == 5){ //Greater omni mage
-            name = "Greater Omni Mage";
-            suitPreference = '\0';
-            minCardValue = 7;
-            maxCardValue = 15;
-        }
-        else {
-            throw new IndexOutOfBoundsException("No enemy exists with the given id");
-        }
-    }
-
-    public int getLevel(){
-        return level;
-    }
-
-    @Override
-    public int getCurrentHealth() {
-        return currentHealth;
-    }
-
-    @Override
-    public void setHealth(int health) {
-        currentHealth = health;
-    }
-
-    public String getName(){
-        return name;
-    }
-    public char getPreferredSuit(){
-        return suitPreference;
-    }
-    public int getMinimumPreferredValue(){
-        return minCardValue;
-    }
-    public int getMaximumPreferredValue(){
-        return maxCardValue;
-    }
+public interface Enemy {
+    int getLevel();
+    int getCurrentHealth();
+    void setHealth(int health);
+    String getName();
+    char getPreferredSuit();
+    int getMinimumPreferredValue();
+    int getMaximumPreferredValue();
 }
--- src/entity/EnemyFactory.java
+++ src/entity/EnemyFactory.java
@@ -1,12 +1,5 @@
 package entity;
 
-import java.util.Random;
-
-public class EnemyFactory implements CharacterFactory {
-
-    @Override
-    public Character create(int level) {
-        Random random_id = new Random();
-        return new Enemy(random_id.nextInt(0,6), level);
-    }
+public interface EnemyFactory  {
+    Enemy create(int id, int level);
 }
