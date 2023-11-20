6ab2ab6a4e80a99f4ee7f71b9b1aed7080c0e8fa
mikaylapradeepan <mikaylapradeepan@gmail.com>
2023-11-20 10:36:27 -0500

    DefendButtonPresenter done.

--- src/interface_adapter/turn_select/DefendButtonPresenter.java
+++ src/interface_adapter/turn_select/DefendButtonPresenter.java
@@ -1,4 +1,37 @@
 package interface_adapter.turn_select;
 
-public class DefendButtonPresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.DefendButton.DefendButtonOutputBoundary;
+import view.in_battle.BattleResultView;
+
+public class DefendButtonPresenter implements DefendButtonOutputBoundary {
+    private final BattleResultViewModel battleResultViewModel;
+    private final TurnSelectViewModel turnSelectViewModel;
+    private final ViewManagerModel viewManagerModel;
+    public DefendButtonPresenter(BattleResultViewModel battleResultViewModel, TurnSelectViewModel turnSelectViewModel, ViewManagerModel viewManagerModel){
+        this.turnSelectViewModel = turnSelectViewModel;
+        this.battleResultViewModel = battleResultViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+    @Override
+    public void prepareDefendSuccessView() {
+        BattleResultState battleResultState = battleResultViewModel.getState();
+        TurnSelectState turnSelectState = turnSelectViewModel.getState();
+        this.battleResultViewModel.setState(battleResultState);
+        this.turnSelectViewModel.setState(turnSelectState);
+
+        battleResultViewModel.firePropertyChanged();
+        turnSelectViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(turnSelectViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+
+    }
+
+    @Override
+    public void prepareDefendFailView(String error) {
+        TurnSelectState turnSelectState = turnSelectViewModel.getState();
+        turnSelectState.getClearError(error);
+        turnSelectViewModel.firePropertyChanged();
+    }
 }
