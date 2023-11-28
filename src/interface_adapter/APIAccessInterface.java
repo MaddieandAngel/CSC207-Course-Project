e4b72845f0fc34e009093ce3b10c91cf87c76a81
Sarah Lloyd-Smith <sarah.lloydsmith@mail.utoronto.ca>
2023-11-28 14:18:29 -0500

    Added DeckInterface as an instance attribute for APIAccess, so now decks and deckIDs are no longer needed as parameters for any APIAccess methods. Changed the tests accordingly and removed the tests that considered the case of the wrong deckID being provided since that is no longer a possible scenario (further looking at the tests may be required)

--- src/interface_adapter/APIAccessInterface.java
+++ src/interface_adapter/APIAccessInterface.java
@@ -7,23 +7,23 @@ import java.io.IOException;
 
 public interface APIAccessInterface {
 
-    public DeckInterface NewDeck() throws IOException, RuntimeException;
+    public void Shuffle() throws IOException, RuntimeException;
 
-    public void Shuffle(DeckInterface deck) throws IOException, RuntimeException;
+    public void DrawCard(String pileName) throws IOException, RuntimeException;
 
-    public void DrawCard(DeckInterface deck, String pileName) throws IOException, RuntimeException;
+    public void CardPlayed(String pileName, String cardCode) throws IOException, RuntimeException;
 
-    public void CardPlayed(String deckID, String pileName, String cardCode) throws IOException, RuntimeException;
+    public void AddToPile(String pileName, String cardCode) throws IOException, RuntimeException;
 
-    public void AddToPile(String deckID, String pileName, String cardCode) throws IOException, RuntimeException;
+    public void MovePileToDeck(String pileName) throws IOException, RuntimeException;
 
-    public void MovePileToDeck(DeckInterface deck, String pileName) throws IOException, RuntimeException;
-
-    public String[] GetCardsInPile(DeckInterface deck, String pileName) throws IOException, RuntimeException;
+    public String[] GetCardsInPile(String pileName) throws IOException, RuntimeException;
 
     public String GetCardImage(String cardCode);
 
     public int GetCardValue(String cardCode);
 
     public char GetCardSuit(String cardCode);
+
+    public DeckInterface getDeck();
     }
