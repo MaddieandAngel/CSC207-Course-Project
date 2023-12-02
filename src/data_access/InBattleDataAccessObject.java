31d034f1909bcc8f14073322ee60a8e24b714c01
Sarah Lloyd-Smith <sarah.lloydsmith@mail.utoronto.ca>
2023-12-01 22:12:10 -0500

    Deleted SaveDeckDataAccessInterface since it is no longer needed due to the new implementation of APIAccess.

--- src/data_access/InBattleDataAccessObject.java
+++ src/data_access/InBattleDataAccessObject.java
@@ -1,21 +1,16 @@
 package data_access;
 
-import entity.Deck;
 import entity.Enemy;
 import entity.Player;
 import entity.PlayerFactory;
 import interface_adapter.APIAccessInterface;
-import interface_adapter.TitleScreen.SaveDeckDataAccessInterface;
 import interface_adapter.explore.GenerateEnemyDataAccessInterface;
 import use_case.AttackButton.AttackButtonDataAccessInterface;
 import use_case.AttackButton.BackButton.BackButtonDataAccessInterface;
 import use_case.AttackButton.CardButton.CardButtonDataAccessInterface;
-import use_case.EnemyBehaviour.EnemyBehaviour;
 import use_case.EnemyBehaviour.EnemyBehaviourInterface;
 
-import java.io.IOException;
-
-public class InBattleDataAccessObject implements GenerateEnemyDataAccessInterface, SaveDeckDataAccessInterface, CardButtonDataAccessInterface, BackButtonDataAccessInterface, AttackButtonDataAccessInterface {
+public class InBattleDataAccessObject implements GenerateEnemyDataAccessInterface, CardButtonDataAccessInterface, BackButtonDataAccessInterface, AttackButtonDataAccessInterface {
 
     private Player player;
     private Enemy enemy;
