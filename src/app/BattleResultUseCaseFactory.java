71e233ba9d9160187bc910b94ce2225030e6914e
mikaylapradeepan <mikaylapradeepan@gmail.com>
2023-12-03 05:09:46 -0500

    BattleResultUseCaseFactory implemented

--- src/app/BattleResultUseCaseFactory.java
+++ src/app/BattleResultUseCaseFactory.java
@@ -2,22 +2,36 @@ package app;
 
 import interface_adapter.BattleResult.BattleResultViewModel;
 import interface_adapter.BattleResult.ContinueButtonController;
+import interface_adapter.BattleResult.ContinueButtonPresenter;
+import interface_adapter.GameOver.GameOverViewModel;
 import interface_adapter.ViewManagerModel;
+import interface_adapter.WinBattle.WinBattleViewModel;
+import interface_adapter.turn_select.TurnSelectViewModel;
+import use_case.BattleResultContinueButton.BattleResultContinueInputBoundary;
+import use_case.BattleResultContinueButton.BattleResultContinueInteractor;
+import use_case.BattleResultContinueButton.BattleResultContinueOutputBoundary;
 import view.in_battle.BattleResultView;
 
 public class BattleResultUseCaseFactory {
     private BattleResultUseCaseFactory(){}
 
-    public static BattleResultView create(ViewManagerModel viewManagerModel, BattleResultViewModel battleResultViewModel){
+    public static BattleResultView create(ViewManagerModel viewManagerModel, BattleResultViewModel battleResultViewModel,
+                                          TurnSelectViewModel turnSelectViewModel, GameOverViewModel gameOverViewModel,
+                                          WinBattleViewModel winBattleViewModel){
 
-        //TODO: finish implementation
-        ContinueButtonController continueButtonController = createContinueButtonUseCase();
+        ContinueButtonController continueButtonController = createContinueButtonUseCase(viewManagerModel, battleResultViewModel,
+                turnSelectViewModel, gameOverViewModel, winBattleViewModel);
 
         return new BattleResultView(battleResultViewModel, continueButtonController);
     }
 
-    private static ContinueButtonController createContinueButtonUseCase(){
-        //TODO: implement properly
-        return new ContinueButtonController();
+    private static ContinueButtonController createContinueButtonUseCase(ViewManagerModel viewManagerModel, BattleResultViewModel battleResultViewModel,
+                                                                        TurnSelectViewModel turnSelectViewModel, GameOverViewModel gameOverViewModel,
+                                                                        WinBattleViewModel winBattleViewModel){
+
+        BattleResultContinueOutputBoundary battleResultContinueOutputBoundary = new ContinueButtonPresenter(turnSelectViewModel,
+                battleResultViewModel, gameOverViewModel, winBattleViewModel, viewManagerModel);
+        BattleResultContinueInputBoundary battleResultContinueInteractor = new BattleResultContinueInteractor(battleResultContinueOutputBoundary);
+        return new ContinueButtonController(battleResultContinueInteractor);
     }
 }
