1eb703412f8c7f848b073804fdf5d91af3f96bf4
mikaylapradeepan <mikaylapradeepan@gmail.com>
2023-12-03 03:00:11 -0500

    Added dataAccessObject argument. Not too sure if I created a 4x4 floor correctly

--- Tests/use_case/PlayButton/PlayButtonInteractorTest.java
+++ Tests/use_case/PlayButton/PlayButtonInteractorTest.java
@@ -1,21 +1,28 @@
 package use_case.PlayButton;
 
-import entity.CurrentFloor;
-import entity.MapBuilder;
+import data_access.ExploreDataAccessObject;
+import entity.*;
+import interface_adapter.stairs.StairsDataAccessInterface;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import use_case.DefendButton.DefendButtonInputBoundary;
+import use_case.DefendButton.DefendButtonInteractor;
 import use_case.movement.MovementOutputData;
 
 import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayButtonInteractorTest {
+    StairsDataAccessInterface dataAccessObject;
     int numberOfRooms;
+    String directions;
     @BeforeEach
     void setup() throws IOException{
+        dataAccessObject = new ExploreDataAccessObject(new CurrentFloorFactory());
         //Generate initial floor (4 by 4 map)
         CurrentFloor floor = new CurrentFloor(4, 4);
         numberOfRooms = floor.getColumns() * floor.getRows();
+        directions = dataAccessObject.MoveToNextFloor(4,4);
     }
     @Test
     void canExplore() throws IOException{
@@ -23,7 +30,7 @@ public class PlayButtonInteractorTest {
             @Override
             public void prepareSuccessView(MovementOutputData movementOutputData) {
                 //Assert if current room has more than 2 directions
-                assert movementOutputData.getDirections().length() > 1;
+                assert directions.length() > 1;
                 //Assert that there are 16 rooms (4 rows and 4 columns)
                 assertEquals(numberOfRooms, 16);
             }
@@ -33,5 +40,7 @@ public class PlayButtonInteractorTest {
                 fail("Fail not expected");
             }
         };
+        PlayButtonInputBoundary interactor = new PlayButtonInteractor(playButtonOutputBoundary, dataAccessObject);
+        interactor.execute();
     }
 }
