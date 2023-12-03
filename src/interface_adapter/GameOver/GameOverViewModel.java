22e8aee54daf90f80de7dde661944328083e4b2b
mikaylapradeepan <mikaylapradeepan@gmail.com>
2023-12-03 03:14:46 -0500

    GameOverViewModel implemented

---
+++ src/interface_adapter/GameOver/GameOverViewModel.java
@@ -0,0 +1,30 @@
+package interface_adapter.GameOver;
+
+import interface_adapter.ViewModel;
+
+import javax.swing.text.View;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class GameOverViewModel extends ViewModel{
+    public static final String PLAY_AGAIN_LABEL = "Play Again";
+    public static final String RETURN_TO_TITLE_LABEL = "Return to Title Screen";
+    private GameOverState state = new GameOverState();
+
+    public GameOverViewModel() {
+        super("game over");
+    }
+    public void setState(GameOverState state){this.state = state;}
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+    public GameOverState getState(){return state;}
+}
