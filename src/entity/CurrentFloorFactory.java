4303bc2cd0a555f1e58670cec511e9535c144d80
MaddieandAngel <madeleine.morais@mail.utoronto.ca>
2023-12-03 21:09:43 -0500

    Made MapBuilder fit the builder design pattern more

--- src/entity/CurrentFloorFactory.java
+++ src/entity/CurrentFloorFactory.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 public class CurrentFloorFactory implements FloorFactory{
     @Override
     public Floor create(int columns, int rows, MapBuilderInterface mapBuilder) {
-        ArrayList<Room> map = new ArrayList<>(columns * rows);
         int currentRoom = -1;
 
         while (currentRoom == -1) { //Generates random floors until one with at least two viable rooms is created
 
-            mapBuilder.buildFirstRow(map);
+            mapBuilder.buildFirstRow();
 
             for (int row_num = 1; row_num < rows - 1; row_num++) {
-                mapBuilder.buildOtherRows(map, row_num * columns);
+                mapBuilder.buildOtherRows(row_num * columns);
             }
-            mapBuilder.buildFinalRow(map, (rows - 1) * columns);
+            mapBuilder.buildFinalRow((rows - 1) * columns);
 
-            currentRoom = mapBuilder.assignAll(map);
+            currentRoom = mapBuilder.assignAll();
         }
+        ArrayList<Room> map = mapBuilder.getMap();
         return new CurrentFloor(columns, rows, map, currentRoom);
     }
 }
--- src/entity/MapBuilderInterface.java
+++ src/entity/MapBuilderInterface.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 public interface MapBuilderInterface {
 
-    int assignAll(ArrayList<Room> map);
-    void buildFirstRow(ArrayList<Room> map);
-    void buildOtherRows(ArrayList<Room> map, int startIndex);
-    void buildFinalRow(ArrayList<Room> map, int startIndex);
+    int assignAll();
+    void buildFirstRow();
+    void buildOtherRows(int startIndex);
+    void buildFinalRow(int startIndex);
+    ArrayList<Room> getMap();
 }
